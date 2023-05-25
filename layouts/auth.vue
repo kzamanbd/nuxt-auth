54d88b03bff925785ce535c1a8c87970ab91e3b7
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-05-25 22:17:01 +0600

    update config

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -51,7 +51,12 @@
 
 								<template #content>
 									<DropdownLink href="/"> Profile </DropdownLink>
-									<DropdownLink @click="logout" method="post" as="button"> Log Out </DropdownLink>
+									<button
+										type="button"
+										@click="logout"
+										class="block w-full px-4 py-2 text-left text-sm leading-5 text-gray-700 dark:text-gray-300 hover:bg-gray-100 dark:hover:bg-gray-800 focus:outline-none focus:bg-gray-100 dark:focus:bg-gray-800 transition duration-150 ease-in-out">
+										Log Out
+									</button>
 								</template>
 							</Dropdown>
 						</div>
@@ -104,7 +109,12 @@
 
 					<div class="mt-3 space-y-1">
 						<ResponsiveNavLink href="/"> Profile </ResponsiveNavLink>
-						<ResponsiveNavLink @click="logout" method="post" as="button"> Log Out </ResponsiveNavLink>
+						<button
+							type="button"
+							@click="logout"
+							class="block w-full pl-3 pr-4 py-2 border-l-4 border-transparent text-left text-base font-medium text-gray-600 dark:text-gray-400 hover:text-gray-800 dark:hover:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-700 hover:border-gray-300 dark:hover:border-gray-600 focus:outline-none focus:text-gray-800 dark:focus:text-gray-200 focus:bg-gray-50 dark:focus:bg-gray-700 focus:border-gray-300 dark:focus:border-gray-600 transition duration-150 ease-in-out">
+							Log Out
+						</button>
 					</div>
 				</div>
 			</div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,11 +4,8 @@ export default defineNuxtConfig({
 	modules: ['@nuxtjs/tailwindcss'],
     runtimeConfig: {
 		public: {
-			backendUrl: 'http://localhost:8000',
+			backendUrl: 'http://localhost:8000/api',
 			frontendUrl: 'http://localhost:3000'
 		}
 	},
-	imports: {
-		dirs: ['./utils']
-	}
 });
