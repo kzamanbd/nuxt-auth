3d8f21dabe50f68e3aae75df6eb4a002d4e2cf8c
KAMRUZZAMAN <kzamanbn@gmail.com>
2023-05-26 01:23:51 +0600

    Update auth.vue

--- layouts/auth.vue
+++ layouts/auth.vue
@@ -33,7 +33,7 @@
 										<button
 											type="button"
 											class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-											{{ user.name }}
+											{{ user?.name }}
 
 											<svg
 												class="ml-2 -mr-0.5 h-4 w-4"
@@ -102,9 +102,9 @@
 				<div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
 					<div class="px-4">
 						<div class="font-medium text-base text-gray-800 dark:text-gray-200">
-							{{ user.name }}
+							{{ user?.name }}
 						</div>
-						<div class="font-medium text-sm text-gray-500">{{ user.email }}</div>
+						<div class="font-medium text-sm text-gray-500">{{ user?.email }}</div>
 					</div>
 
 					<div class="mt-3 space-y-1">
